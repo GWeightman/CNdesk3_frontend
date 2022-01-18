eace37e749d205caada7040bee0682fc9da6921e
GWeightman <g.weightman@hotmail.co.uk>
2022-01-18 15:20:16 +0000

    contact us page has been fleshed out

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom"
+
 const Contact = () => {
     return(
         <div>
-            <p>this is a contact page</p>
+            <h1>Place a job request with Node Nation</h1>
+            <input placeholder='client name'/>
+            <input placeholder="contact number"/>
+            <input placeholder="email"/>
+            <input placeholder="language"/>
+            <input placeholder="pay range"/>
+            <input placeholder="job description"/>
+            <button>send</button>
+            <button><Link to={"/"}>go back</Link></button>
         </div>
     )
 }
