59db5eba704d30a268a2429b1ca554376d1c47fb
GWeightman <g.weightman@hotmail.co.uk>
2022-01-18 15:51:04 +0000

    home padded out and folder deleted

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,12 +4,20 @@ const Contact = () => {
     return(
         <div>
             <h1>Place a job request with Node Nation</h1>
+            <p>Your Name</p>
             <input placeholder='client name'/>
+            <p>Contact Number</p>
             <input placeholder="contact number"/>
+            <p>Email</p>
             <input placeholder="email"/>
+            <p>If you know what language you would like to use</p>
             <input placeholder="language"/>
+            <p>How much would you like be looking at paying</p>
             <input placeholder="pay range"/>
+            <p>Give a description of the project brief</p>
             <input placeholder="job description"/>
+            <br/>
+            <br/>
             <button>send</button>
             <button><Link to={"/"}>go back</Link></button>
         </div>
--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link, Navigate, Router } from "react-router-dom"
+
+const login_handler = (e) => {
+    e.preventDefault();
+    <Link to={"/login"}/>
+}
 
 const Home = () => {
     return(
         <div>
-           <h1> home </h1>
            <button><Link to={"/contact"}>contact us</Link></button>
-           <button><Link to={"/login"}>log in</Link></button> 
+           <button onClick={login_handler}>log in</button> 
+           <h1> Welcome to Node Nation </h1>
+           <p>A place to bring small coding projects and independent developers together</p>
+           
         </div>
     )
 }
