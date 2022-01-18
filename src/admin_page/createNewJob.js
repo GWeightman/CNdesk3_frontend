f4ca7f14ddbf823e0324ebdef3852beb0dfe9ce7
Dimaltas <d.maltas@hotmail.com>
2022-01-18 17:11:28 +0000

    added more buttons

---
+++ src/admin_page/createNewJob.js
@@ -0,0 +1,36 @@
+import react from "react";
+import { Link } from "react-router-dom";
+import Admin from "./Admin";
+
+const Create = () =>{
+    return (
+        <h1>Create a new job</h1>
+    )
+    //     const handleSubmit = event => {
+    //      event.preventDefault();
+    //      alert('You have submitted the form.')
+    //    }
+      
+    //     return(
+    //         <div>
+    //             <h1 className="header">Create a new job</h1>
+    //             <Create/>
+                
+    //       <div className="wrapper">
+    //         <h1>How About Them Apples</h1>
+    //         <form onSubmit={handleSubmit}>
+    //           <fieldset>
+    //             <label>
+    //               <p>Name</p>
+    //               <input name="name" />
+    //             </label>
+    //           </fieldset>
+    //           <button type="submit">Submit</button>
+    //         </form>
+    //       </div>
+    //       </div>
+    //     )
+}
+
+
+export default Create;
