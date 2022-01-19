f8521a7f733db37709068ad88c742affee1a97d3
Dimaltas <d.maltas@hotmail.com>
2022-01-19 16:50:18 +0000

    added useState at createNewJob

--- src/admin_page/createNewJob.js
+++ src/admin_page/createNewJob.js
@@ -1,6 +1,13 @@
+import React, {useState} from 'react';
 import { createNewJobFetch } from "../utilities/fetches";
 
-const Create = (clientname, description, salary, phonenumber, email) =>{
+const Create = ({clientname, description, salary, phonenumber, email}) =>{
+  const [clientname, setClientName] = useState();
+  const [description, setDescription] = useState();
+  const [salary, setSalary] = useState();
+  const [phonenumber, setPhoneNumber] = useState();
+  const [email, setEmail] = useState();
+
   const createJobHandler = async (e) => {
     e.preventDefault();
     await createNewJobFetch(clientname, description, salary, phonenumber, email)
@@ -12,28 +19,20 @@ const Create = (clientname, description, salary, phonenumber, email) =>{
       
         return(
             <div>
-                <h1 className="header">Create a new job</h1>
-                
-          <div className="wrapper">
-            <h1>How About Them Apples</h1>
+                <h1 className="header">Create a new job</h1>  
             <form onSubmit={createJobHandler}>
-              <fieldset>
-                <label>
                   <p>Client name</p>
-                  <input title="title" />
+                  <input onChange = {(e) => setClientName(e.target.value)} placeholder = 'clientname' />
                   <p>Description</p>
-                  <input description="description" />
+                  <input onChange = {(e) => setDescription(e.target.value)} placeholder = 'description' />
                   <p>Salary</p>
-                  <input salary = "salary" />
+                  <input onChange = {(e) => setSalary(e.target.value)} placeholder = 'salary' />
                   <p>Number</p>
-                  <input description= "number" />
+                  <input onChange = {(e) => setPhoneNumber(e.target.value)} placeholder = 'phonenumber' />
                   <p>Email</p>
-                  <input email = "email" />
-                </label>
-              </fieldset>
+                  <input onChange = {(e) => setEmail(e.target.value)} placeholder = 'email' />
               <button type="submit">Submit</button>
             </form>
-          </div>
           </div>
         )
 }
