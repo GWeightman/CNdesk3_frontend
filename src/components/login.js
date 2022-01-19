dbdd5773e1735711c356b2f592682bba4058249a
GWeightman <g.weightman@hotmail.co.uk>
2022-01-19 11:24:06 +0000

    login added and fixed

--- src/components/login.js
+++ src/components/login.js
@@ -1,14 +1,25 @@
 import { Link } from "react-router-dom";
 import { login_fetch } from "../utilities/fetches";
 
-const Login = ({setUsername, setPassword}) => {
+const Login = ({username, setUsername, password, setPassword}) => {
 
-    const login_handler = async (e, username, password) => {
+    const login_handler = async (e) => {
         e.preventDefault();
-        const ret_val = await login_fetch(username)
-        if (ret_val.usr.username == username && ret_val.user.password == password) {
-          console.log(ret_val);
+        const ret_val = await login_fetch(username, password)
+        console.log(ret_val.password)
+        if (ret_val.username === username) {
+          if(ret_val.status == "admin"){
+              window.location.href = "/admin"
+          } else {
+              window.location.href = "/devpage"
+          }
+        } else {
+            window.location.href = "/notauth"
         }
+    }
+
+    const signup_button = () => {
+        window.location.href = "/signup"
     }
 
     return(
@@ -23,7 +34,7 @@ const Login = ({setUsername, setPassword}) => {
                 <br/>
                 <br/>
                 <button type='submit'>submit</button>
-                <button><Link to={"/signup"}>sign up</Link></button>
+                <button onClick={signup_button}>sign up</button>
             </form>
         </div>
     )
