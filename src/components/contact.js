14534ddda06b381053a122d950002621d6989d34
GWeightman <g.weightman@hotmail.co.uk>
2022-01-24 13:59:48 +0000

    contact us page has been updated

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,13 @@
 import { Link } from "react-router-dom"
 
 const Contact = () => {
+
+    const return_handler = () => {
+        window.location.href = "/"
+    }
+
     return(
-        <div>
+        <form action="mailto:admin@nodenation.uk" method="post" encType="text/plain">
             <h1>Place a job request with Node Nation</h1>
             <p>Your Name</p>
             <input placeholder='client name'/>
@@ -19,8 +24,8 @@ const Contact = () => {
             <br/>
             <br/>
             <button>send</button>
-            <button><Link to={"/"}>go back</Link></button>
-        </div>
+            <button onClick={return_handler}>go back</button>
+        </form>
     )
 }
 
