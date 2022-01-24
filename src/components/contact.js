35c6089ed79c2c927c1c0796942f351500660a2b
GWeightman <g.weightman@hotmail.co.uk>
2022-01-24 14:16:16 +0000

    css folder addded

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,25 +7,28 @@ const Contact = () => {
     }
 
     return(
-        <form action="mailto:admin@nodenation.uk" method="post" encType="text/plain">
-            <h1>Place a job request with Node Nation</h1>
-            <p>Your Name</p>
-            <input placeholder='client name'/>
-            <p>Contact Number</p>
-            <input placeholder="contact number"/>
-            <p>Email</p>
-            <input placeholder="email"/>
-            <p>If you know what language you would like to use</p>
-            <input placeholder="language"/>
-            <p>How much would you like be looking at paying</p>
-            <input placeholder="pay range"/>
-            <p>Give a description of the project brief</p>
-            <input placeholder="job description"/>
-            <br/>
-            <br/>
-            <button>send</button>
+        <div>
+            <form action="mailto:admin@nodenation.uk" method="post" encType="text/plain">
+                <h1>Place a job request with Node Nation</h1>
+                <p>Your Name</p>
+                <input placeholder='client name'/>
+                <p>Contact Number</p>
+                <input placeholder="contact number"/>
+                <p>Email</p>
+                <input placeholder="email"/>
+                <p>If you know what language you would like to use</p>
+                <input placeholder="language"/>
+                <p>How much would you like be looking at paying</p>
+                <input placeholder="pay range"/>
+                <p>Give a description of the project brief</p>
+                <input placeholder="job description"/>
+                <br/>
+                <br/>
+            </form>
+
+            <button type="submit">send</button>
             <button onClick={return_handler}>go back</button>
-        </form>
+        </div>
     )
 }
 
