b74dce716b525c1df8fcb78c4e58eb701d0ae8b1
Dimaltas <d.maltas@hotmail.com>
2022-01-24 17:21:40 +0000

    we can now find the users in groups! gg

--- src/components/listedJobs.js
+++ src/components/listedJobs.js
@@ -15,26 +15,29 @@ const ListedJobs = ({clientname, description, salary, phonenumber, email, jobs, 
     console.log(null)
     return null
   }
-    return(
-        <div>
-          <h1>Here you can find all the listed jobs</h1>
-          <br></br>
-          <div>  
+  const returnHandler = () => {
+    window.location.href = "/admin"
+  }
+  return(
+      <div>
+        <h1>Here you can find all the listed jobs</h1>
+        <br/>
+        <div>  
           {jobs.map((job,index) =>{
             return(
               <div key={index}>
-            <p>{job.clientname}</p>
-            <p>{job.contact_number}</p>
-            <p>{job.email}</p>
-            <p>{job.language}</p>
-            <p>{job.pay_range}</p>
-            <p>{job.job_description}</p>
-            <br/>
-            </div>
-        )})}
-          </div>
-          <button>Find a job</button>
-        </div>      
-    )
+                <p>{job.clientname}</p>
+                <p>{job.contact_number}</p>
+                <p>{job.email}</p>
+                <p>{job.language}</p>
+                <p>{job.pay_range}</p>
+                <p>{job.job_description}</p>
+                <br/>
+              </div>
+            )})}
+        </div>
+        <button onClick={returnHandler}>Go back to homepage</button>
+      </div>      
+  )
 }
 export default ListedJobs;
