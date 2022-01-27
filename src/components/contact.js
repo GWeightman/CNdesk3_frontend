fe3151000796ee7604895861926b9e27557109f7
lujohnny95 <lu_johnny95@yahoo.com>
2022-01-27 10:27:01 +0000

    Home, login, signup, contact styles added

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import network from "../images/network.jpg";
 
 const Contact = () => {
 
@@ -8,21 +9,31 @@ const Contact = () => {
 
     return(
         <div>
-            <h1>Place a job request with Node Nation</h1>
-            <form action="mailto:admin@nodenation.uk" subject="Job%Request" method="post" encType="text/plain">
-                Username:<br/><input type="text" name="name" required/><br/>
-                Contact Number:<br/><input type="number" name="contact number"/><br/>
-                Email:<br/><input type="text" name="email" required/><br/>
-                Coding Language:<br/><input type="text" name="language"/><br/>
-                Pay Range:<br/><input type="number" name="pay range"/><br/>
-                Job Description:<br/><textarea type="text" name="job description" required></textarea>
-                <br/>
-                <br/>
-                <input type="submit" value="Send"/>
-                <button onClick={return_handler}>Go Back</button>
-            </form>
-
-            
+            <div class="home-wrapper" style={{ backgroundImage: `url(${network})` }}>
+                <header>
+                <div class="logo-area">
+                    <h1 class="logo-title">NODE NATION</h1>
+                </div>
+                </header>
+                <div className="contact-section">
+                    <h2>Place a job request with Node Nation</h2>
+                    <form action="mailto:admin@nodenation.uk" subject="Job%Request" method="post" encType="text/plain">
+                        Username:<br/><input type="text" name="name" required/><br/>
+                        Contact Number:<br/><input type="number" name="contact number"/><br/>
+                        Email:<br/><input type="text" name="email" required/><br/>
+                        Coding Language:<br/><input type="text" name="language"/><br/>
+                        Pay Range:<br/><input type="number" name="pay range"/><br/>
+                        Job Description:<br/><textarea type="text" name="job description" required></textarea>
+                        <br/>
+                        <br/>
+                        <input className="signup-btn" type="submit" value="Send"/>
+                        <button className="signup-btn" onClick={return_handler}>Go Back</button>
+                    </form>
+                </div>
+            </div>
+            <footer>
+                <p class="footer-text">NodeNation - 2022</p>
+            </footer>
         </div>
     )
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,23 +1,46 @@
 import "../styles/home.css"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
+import network from "../images/network.jpg";
 
 const Home = () => {
     return(
+        // <div>
+        //     <div className="welcome">
+        //         <h1> Welcome to Node Nation!</h1>
+        //         <p>A place to bring small coding projects and independent developers together</p>
+        //         <br/>
+        //         <br/>
+        //         <button><Link to="/contact">contact us</Link></button>
+        //         <button><Link to="/login">log in</Link></button>
+        //     </div>
+        //     <div className="devbutton">
+        //         <p>These buttons are for development only and must be deleted before submission</p>
+        //         <button><Link to="/admin">Admin</Link></button>
+        //         <button><Link to="/mainpage">Developer</Link></button>
+        //     </div>
+        // </div>
+
         <div>
-            <div className="welcome">
-                <h1> Welcome to Node Nation </h1>
-                <p>A place to bring small coding projects and independent developers together</p>
-                <br/>
-                <br/>
-                <button><Link to="/contact">contact us</Link></button>
-                <button><Link to="/login">log in</Link></button>
-            </div>
-            <div className="devbutton">
-                <p>These buttons are for development only and must be deleted before submission</p>
-                <button><Link to="/admin">Admin</Link></button>
-                <button><Link to="/mainpage">Developer</Link></button>
-            </div>
+      <div class="home-wrapper" style={{ backgroundImage: `url(${network})` }}>
+        <header>
+          <div class="logo-area">
+            <h1 class="logo-title">NODE NATION</h1>
+            <ul class="nav-area">
+                <li><NavLink className="link" to="/contact">Contact Us</NavLink></li>
+                <li><NavLink className="link" to="/login">Login</NavLink></li>
+            </ul>
+          </div>
+        </header>
+        <div class="welcome-text">
+          <h1>Welcome to NodeNation!</h1>
+          <h3>Join, connect and create with clients and developers!</h3>
+          <button class="start-btn"><Link className="link" to="/signup">Get started!</Link></button>
         </div>
+      </div>
+      <footer>
+        <p class="footer-text">NodeNation - 2022</p>
+      </footer>
+    </div>
     )
 }
 
